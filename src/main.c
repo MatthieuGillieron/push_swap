840682efbee70da88af80a8946d099ec3a5a4298
MatthieuGillieron <gillieron.matthieu@ik.me>
2025-02-26 10:30:42 +0100

    test(main): Add test

--- src/main.c
+++ src/main.c
@@ -6,7 +6,49 @@
 /*   By: mg <mg@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 15:11:39 by mg                #+#    #+#             */
-/*   Updated: 2025/02/12 15:11:40 by mg               ###   ########.fr       */
+/*   Updated: 2025/02/26 10:29:48 by mg               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int	is_sorted(t_stack *st)
+{
+	t_stack	*s;
+
+	s = st;
+	while (s->next)
+	{
+		if (s->n > s->next->n)
+			return (0);
+		s = s->next;
+	}
+	return (1);
+}
+
+static void	push_swap(t_stack **sa, t_stack **sb, int size)
+{
+	if (size == 2 && !is_sorted(*sa))
+		swap_move(sa, NULL, "sa");
+	else if (size == 3)
+		small_sort(sa);
+	else if (size > 3 && !is_sorted(*sa))
+		big_sort(sa, sb);
+}
+
+int	main(int ac, char **av)
+{
+	int		size;
+	t_stack	*sa;
+	t_stack	*sb;
+
+	if (ac < 2)
+		return (-1);
+	check_args(ac, av);
+	sa = create_stack(ac, av);
+	sb = NULL;
+	size = get_stack_size(sa);
+	get_main_index(sa, size);
+	push_swap(&sa, &sb, size);
+	free_stack(&sa);
+	free_stack(&sb);
+	return (0);
+}
--- src/parsing/parse_args.c
+++ src/parsing/parse_args.c
@@ -6,9 +6,92 @@
 /*   By: mg <mg@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/13 12:02:04 by mg                #+#    #+#             */
-/*   Updated: 2025/02/13 12:38:44 by mg               ###   ########.fr       */
+/*   Updated: 2025/02/26 10:26:51 by mg               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header/push_swap.h"
 
+#include "push_swap.h"
+
+int	check_limit(long num)
+{
+	if (num > INT_MAX || num < INT_MIN)
+	{
+		ft_putstr_fd("Error\n", 2);
+		return (0);
+	}
+	return (1);
+}
+
+int	check_double(int num, char **args, int i)
+{
+	i += 1;
+	while (args[i])
+	{
+		if (num == ft_atoi(args[i]))
+		{
+			ft_putstr_fd("Error\n", 2);
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+
+int	check_is_num(char *av)
+{
+	int	y;
+
+	y = 0;
+	while (av[y])
+	{
+		if (av[y] == '+' || av[y] == '-')
+			y++;
+		if (!ft_isdigit((av[y])))
+		{
+			ft_putstr_fd("Error\n", 2);
+			return (0);
+		}
+		y++;
+	}
+	return (1);
+}
+
+int	validate_args(char **tmp, char **tmp_bis)
+{
+	int	i;
+
+	i = 0;
+	while (tmp[i])
+	{
+		if (!check_limit(ft_atoi(tmp[i])) || \
+			!check_double(ft_atoi(tmp[i]), tmp, i) || \
+			!check_is_num(tmp[i]))
+			free_and_exit(tmp_bis);
+		i++;
+	}
+	return (i);
+}
+
+int	check_args(int ac, char **av)
+{
+	int		i;
+	char	**tmp;
+	char	**tmp_bis;
+
+	tmp_bis = NULL;
+	if (ac == 2)
+	{
+		tmp_bis = ft_split(av[1], ' ');
+		tmp = tmp_bis;
+	}
+	else
+		tmp = av + 1;
+	i = validate_args(tmp, tmp_bis);
+	if (i == 0)
+		free_and_exit(tmp_bis);
+	if (tmp_bis)
+		ft_free(tmp_bis);
+	return (0);
+}
